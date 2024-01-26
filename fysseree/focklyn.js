1db13d4183b86eb91fe2d48ece3a54f39e2dd4fe
peddyrbeg <85888409+peddyrbeg@users.noreply.github.com>
2024-01-26 18:02:03 +0000

    Add files via upload

---
+++ fysseree/focklyn.js
@@ -0,0 +1,42 @@
+const focklynYnsit = {
+    "nane": {
+        caslys: "caslyssyn/nane.webp",
+        sheean: "sheeanyn/nane.mp3"
+    },
+    "jees": {
+        caslys: "caslyssyn/jees.webp",
+        sheean: "sheeanyn/jees.mp3"
+    },
+    "tree": {
+        caslys: "caslyssyn/tree.webp",
+        sheean: "sheeanyn/tree.mp3"
+    },
+    "kiare": {
+        caslys: "caslyssyn/kiare.webp",
+        sheean: "sheeanyn/kiare.mp3"
+    },
+    "queig": {
+        caslys: "caslyssyn/queig.webp",
+        sheean: "sheeanyn/queig.mp3"
+    },
+    "shey": {
+        caslys: "caslyssyn/shey.webp",
+        sheean: "sheeanyn/shey.mp3"
+    },
+    "shiaght": {
+        caslys: "caslyssyn/shiaght.webp",
+        sheean: "sheeanyn/shiaght.mp3"
+    },
+    "hoght": {
+        caslys: "caslyssyn/hoght.webp",
+        sheean: "sheeanyn/hoght.mp3"
+    },
+    "nuy": {
+        caslys: "caslyssyn/nuy.webp",
+        sheean: "sheeanyn/nuy.mp3"
+    },
+    "jeih": {
+        caslys: "caslyssyn/jeih.webp",
+        sheean: "sheeanyn/jeih.mp3"
+    }
+};
